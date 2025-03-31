592f3db5777592869028bb0ab8b16d0a0830e7b1
dvegaa20 <diego.vega.camacho@gmail.com>
2025-03-31 09:33:01 -0600

    feat(main): add main script for lexer token scanning

---
+++ MIyC/main.py
@@ -0,0 +1,11 @@
+from Lexer import *
+import sys
+
+if __name__ == "__main__":
+    lexer = Lexer("test_cases/bad/prog1.txt")
+
+    token = lexer.scan()
+    while token.tag != Tag.EOF:
+        print(str(token))
+        token = lexer.scan()
+    print("END")
